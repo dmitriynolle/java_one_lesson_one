4a5c72c5a9b65cf923d1368062f5a5aa0f465a46
Dmitriy <dimusy@mail.ru>
2020-03-06 17:51:27 +0300

    lesson_two

--- src/ru/geekbrains/java_one_lesson_one/HomeWork.java
+++ src/ru/geekbrains/java_one_lesson_one/HomeWork.java
@@ -1,44 +1,114 @@
 package ru.geekbrains.java_one_lesson_one;
 
+import java.util.Arrays;
+
 public class HomeWork {
 
     public static void main(String[] args) {
-        float one = homeWorkOne(2, 3, 10, 3);
-        boolean two = homeWorkTwo(10, 15);
-        homeWorkThree(0);
-        System.out.println(homeWorkFour("Дмитрий"));
-        homeWorkFive(2020);
+
+//        задание один
+        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println(Arrays.toString(arr1));
+        homeWorkOne(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+//        задание два
+        int[] arr2 = new int[8];
+        homeWorkTwo(arr2);
+        System.out.println(Arrays.toString(arr2));
+
+//        задание три
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println(Arrays.toString(arr3));
+        homeWorkThree(arr3);
+        System.out.println(Arrays.toString(arr3));
+
+//        задание четыре
+        int[] arr4 = new int[10];
+        for (int x = 0; x < arr4.length; x++)
+            arr4[x] = (int) (Math.random() * 100);
+        System.out.println(Arrays.toString(arr4));
+        homeWorkFour(arr4);
+
+//        задание пять
+        int[][] arr5 = new int[10][10];
+        for (int i = 0; i < arr5.length; i++) {
+            arr5[i][i] = 1;
+            arr5[i][arr5.length - 1 - i] = 1;
+            System.out.println(Arrays.toString(arr5[i]));
+        }
+
+//        задание шесть
+        int[] arr6 = new int[5];
+        for (int i = 0; i < arr6.length; i++)
+            arr6[i] = (int) (Math.random() * 3);
+        System.out.println(homeWorkSix(arr6));
+
+//        задание семь
+        int[] arr7 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int n = -2;
+        homeWorkSeven(arr7, n);
+        System.out.println(Arrays.toString(arr7));
     }
 
-    private static void homeWorkFive(int year) {
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            System.out.println("Високосный");
-        } else {
-            System.out.println("Не високосный");
+    private static void homeWorkSeven(int[] arr, int n) {
+        for (int i = 0; i< Math.abs(n); i++){
+            if (n>0){
+                int element = 0;
+                element = arr[0];
+                System.arraycopy(arr, 1, arr, 0, arr.length - 1);
+                arr[arr.length-1] = element;
+            } else if (n<0){
+                int element = 0;
+                element = arr[arr.length-1];
+                System.arraycopy(arr, 0, arr, 1, arr.length - 1);
+                arr[0] = element;
+            }
         }
     }
 
-    private static String homeWorkFour(String name) {
-        return "Привет, " + name + "!";
+    private static boolean homeWorkSix(int[] arr) {
+        boolean rezult = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int right = 0;
+            int left = 0;
+            for (int x = 0; x <= i; x++) {
+                left += arr[x];
+            }
+            for (int y = arr.length - 1; y > i; y--) {
+                right += arr[y];
+            }
+            if (right == left) {
+                rezult = true;
+                break;
+            }
+        }
+        return rezult;
     }
 
-    private static void homeWorkThree(int a) {
-        if (a >= 0) {
-            System.out.println("число положительное");
-        } else {
-            System.out.println("Число отрицательное");
+    private static void homeWorkFour(int[] arr) {
+        int max = arr[0];
+        int min = arr[0];
+        for (int x = 0; x < arr.length; x++) {
+            if (arr[x] > max) max = arr[x];
+            if (arr[x] < min) min = arr[x];
+        }
+        System.out.println("Максимальный элемент: " + max + " Минимадьный элемент: " + min);
+    }
+
+    private static void homeWorkThree(int[] arr) {
+        for (int x = 0; x < arr.length; x++) {
+            if (arr[x] < 6) arr[x] *= 2;
         }
     }
 
-    private static boolean homeWorkTwo(int a, int b) {
-        if (a + b > 10 && a + b < 20) {
-            return true;
-        } else {
-            return false;
-        }
+    private static void homeWorkTwo(int[] arr) {
+        for (int x = 0; x < arr.length; x++)
+            arr[x] = x * 3 + 1;
     }
 
-    private static float homeWorkOne(int a, int b, int c, int d) {
-        return a * (b + (c / d));
+    private static void homeWorkOne(int[] arr) {
+        for (int x = 0; x < arr.length; x++)
+            arr[x] = (arr[x] == 1) ? 0 : 1;
     }
 }
