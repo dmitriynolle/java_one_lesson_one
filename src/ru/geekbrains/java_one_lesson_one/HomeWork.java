ddcd006b996388f844e08e0af84824ffbd2e2851
Dmitriy <dimusy@mail.ru>
2020-03-11 18:03:19 +0300

    lesson_three

--- src/ru/geekbrains/java_one_lesson_one/HomeWork.java
+++ src/ru/geekbrains/java_one_lesson_one/HomeWork.java
@@ -1,114 +1,235 @@
 package ru.geekbrains.java_one_lesson_one;
 
-import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class HomeWork {
 
+    private static final char DOT_HUMAN = 'X';
+    private static final char DOT_AI = 'O';
+    private static final char DOT_EMPTY = '_';
+
+    private static int fieldSizeX;
+    private static int fieldSizeY;
+    private static int winSize;
+    private static char[][] field;
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Random RANDOM = new Random();
+
+    private static void initField() {
+        fieldSizeY = 3;
+        fieldSizeX = 3;
+        winSize = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = DOT_EMPTY;
+            }
+        }
+    }
+
+    private static void printField() {
+        System.out.println("------");
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                System.out.print(field[y][x] + "|");
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean isValidCell(int x, int y) {
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    private static boolean isEmptyCell(int x, int y) {
+        return field[y][x] == DOT_EMPTY;
+    }
+
+    private static void humanTurn() {
+        int x;
+        int y;
+        do {
+            System.out.println("Введите координаты хода X и Y (от 1 до 3) через пробел >>> ");
+            x = SCANNER.nextInt() - 1;
+            y = SCANNER.nextInt() - 1;
+        } while (!isValidCell(x, y) || !isEmptyCell(x, y));
+        field[y][x] = DOT_HUMAN;
+    }
+
+    private static void aiTurn() {
+        if (!checkAI(DOT_AI) && !checkAI(DOT_HUMAN)) {
+            int x;
+            int y;
+            do {
+                x = RANDOM.nextInt(fieldSizeX);
+                y = RANDOM.nextInt(fieldSizeY);
+            } while (!isEmptyCell(x, y));
+            field[y][x] = DOT_AI;
+        }
+    }
+
+    private static boolean checkAI (int c){
+        for (int y = 0; y < fieldSizeY; y++) {
+            int winX = 0;
+            int winY = 0;
+            int winXY1 = 0;
+            int winXY2 = 0;
+            int winYX1 = 0;
+            int winYX2 = 0;
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (field[y][x] == c) {
+                    winX++;
+                    if (winX == winSize - 1) {
+                        for (int i = x + 1; i >= x + 1 - winSize; i--) {
+                            if (isValidCell(i, y) && isEmptyCell(i, y)) {
+                                field[y][i] = DOT_AI;
+                                return true;
+                            }
+                        }
+                    }
+                }
+                if (field[x][y] == c) {
+                    winY++;
+                    if (winY == winSize - 1) {
+                        for (int i = x + 1; i >= x + 1 - winSize; i--) {
+                            if (isValidCell(y, i) && isEmptyCell(y, i)) {
+                                field[i][y] = DOT_AI;
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            for (int x = 0; x < fieldSizeX - y; x++) {
+                if (field[x][x + y] == c) {
+                    winXY1++;
+                    if (winXY1 == winSize - 1) {
+                        int j = x + 1;
+                        for (int i = x + y + 1; i >= x + y + 1 - winSize; i--) {
+                            if (isValidCell(i, j) && isEmptyCell(i, j)) {
+                                field[j][i] = DOT_AI;
+                                return true;
+                            }
+                            j--;
+                        }
+                    }
+                }
+                if (field[x + y][x] == c) {
+                    winXY2++;
+                    if (winXY2 == winSize - 1) {
+                        int j = x + y + 1;
+                        for (int i = x + 1; i >= x + 1 - winSize; i--) {
+                            if (isValidCell(i, j) && isEmptyCell(i, j)) {
+                                field[j][i] = DOT_AI;
+                                return true;
+                            }
+                            j--;
+                        }
+                    }
+                }
+                if (field[x][fieldSizeX - 1 - (x + y)] == c) {
+                    winYX1++;
+                    if (winYX1 == winSize - 1) {
+                        int j = x + 1;
+                        for (int i = fieldSizeX - 1 - (x + y) - 1; i <= fieldSizeX - 1 - (x + y) - 1 + winSize; i++) {
+                            if (isValidCell(i, j) && isEmptyCell(i, j)) {
+                                field[j][i] = DOT_AI;
+                                return true;
+                            }
+                            j--;
+                        }
+                    }
+                }
+                if (field[x + y][fieldSizeX - 1 - x] == c) {
+                    winYX2++;
+                    if (winYX2 == winSize - 1) {
+                        int j = x + y + 1;
+                        for (int i = fieldSizeX - 1 - x - 1; i <= fieldSizeX - 1 - x - 1 + winSize; i++) {
+                            if (isValidCell(i, j) && isEmptyCell(i, j)) {
+                                field[j][i] = DOT_AI;
+                                return true;
+                            }
+                            j--;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isFieldFull() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (field[y][x] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkWin(char c) {
+        for (int y = 0; y < fieldSizeY; y++) {
+            int winY = 0;
+            int winX = 0;
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (field[y][x] == c) winX++;
+                else winX = 0;
+                if (field[x][y] == c) winY++;
+                else winY = 0;
+                if (winX == winSize || winY == winSize) return true;
+            }
+        }
+        for (int y = 0; y < fieldSizeY; y++) {
+            int winXY = 0;
+            int winYX = 0;
+            for (int x = 0; x < fieldSizeX - y; x++) {
+                if (field[x][x + y] == c) winXY++;
+                else if (field[x + y][x] == c) winXY++;
+                else winXY = 0;
+
+                if (field[x][fieldSizeX - 1 - (x + y)] == c) winYX++;
+                else if (field[x + y][fieldSizeX - 1 - x] == c) winYX++;
+                else winYX = 0;if (winXY == winSize || winYX == winSize) return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-
-//        задание один
-        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        System.out.println(Arrays.toString(arr1));
-        homeWorkOne(arr1);
-        System.out.println(Arrays.toString(arr1));
-
-//        задание два
-        int[] arr2 = new int[8];
-        homeWorkTwo(arr2);
-        System.out.println(Arrays.toString(arr2));
-
-//        задание три
-        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        System.out.println(Arrays.toString(arr3));
-        homeWorkThree(arr3);
-        System.out.println(Arrays.toString(arr3));
-
-//        задание четыре
-        int[] arr4 = new int[10];
-        for (int x = 0; x < arr4.length; x++)
-            arr4[x] = (int) (Math.random() * 100);
-        System.out.println(Arrays.toString(arr4));
-        homeWorkFour(arr4);
-
-//        задание пять
-        int[][] arr5 = new int[10][10];
-        for (int i = 0; i < arr5.length; i++) {
-            arr5[i][i] = 1;
-            arr5[i][arr5.length - 1 - i] = 1;
-            System.out.println(Arrays.toString(arr5[i]));
-        }
-
-//        задание шесть
-        int[] arr6 = new int[5];
-        for (int i = 0; i < arr6.length; i++)
-            arr6[i] = (int) (Math.random() * 3);
-        System.out.println(homeWorkSix(arr6));
-
-//        задание семь
-        int[] arr7 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        int n = -2;
-        homeWorkSeven(arr7, n);
-        System.out.println(Arrays.toString(arr7));
-    }
-
-    private static void homeWorkSeven(int[] arr, int n) {
-        for (int i = 0; i< Math.abs(n); i++){
-            if (n>0){
-                int element = 0;
-                element = arr[0];
-                System.arraycopy(arr, 1, arr, 0, arr.length - 1);
-                arr[arr.length-1] = element;
-            } else if (n<0){
-                int element = 0;
-                element = arr[arr.length-1];
-                System.arraycopy(arr, 0, arr, 1, arr.length - 1);
-                arr[0] = element;
-            }
+        while (true) {
+        playOneRound();
+            System.out.println("Play again?");
+            if (SCANNER.next().equals("no"))
+                break;
         }
     }
 
-    private static boolean homeWorkSix(int[] arr) {
-        boolean rezult = false;
-        for (int i = 0; i < arr.length - 1; i++) {
-            int right = 0;
-            int left = 0;
-            for (int x = 0; x <= i; x++) {
-                left += arr[x];
+    private static void playOneRound() {
+        initField();
+        printField();
+        while (true) {
+            humanTurn();
+            printField();
+            if (checkWin(DOT_HUMAN)) {
+                System.out.println("Выйграл человек!!!");
+                break;
             }
-            for (int y = arr.length - 1; y > i; y--) {
-                right += arr[y];
+            if (isFieldFull()) {
+                System.out.println("Ничья!!!");
+                break;
             }
-            if (right == left) {
-                rezult = true;
+            aiTurn();
+            printField();
+            if (checkWin(DOT_AI)) {
+                System.out.println("Выйграл AI!!!");
+                break;
+            }
+            if (isFieldFull()) {
+                System.out.println("Ничья!!!");
                 break;
             }
         }
-        return rezult;
-    }
-
-    private static void homeWorkFour(int[] arr) {
-        int max = arr[0];
-        int min = arr[0];
-        for (int x = 0; x < arr.length; x++) {
-            if (arr[x] > max) max = arr[x];
-            if (arr[x] < min) min = arr[x];
-        }
-        System.out.println("Максимальный элемент: " + max + " Минимадьный элемент: " + min);
-    }
-
-    private static void homeWorkThree(int[] arr) {
-        for (int x = 0; x < arr.length; x++) {
-            if (arr[x] < 6) arr[x] *= 2;
-        }
-    }
-
-    private static void homeWorkTwo(int[] arr) {
-        for (int x = 0; x < arr.length; x++)
-            arr[x] = x * 3 + 1;
-    }
-
-    private static void homeWorkOne(int[] arr) {
-        for (int x = 0; x < arr.length; x++)
-            arr[x] = (arr[x] == 1) ? 0 : 1;
     }
 }
